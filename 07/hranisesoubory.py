73ad9b857dda8aa92a3e335f356eaaaa228d4317
Lea Bradáčová <gedrex@gmail.com>
2016-05-17 19:59:34 +0200

    Opensource, prace se soubory, github

---
+++ 07/hranisesoubory.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.4
+with open('basen') as soubor:
+    for radek in basen:
+        radek = radek.rstrip()
+        print(radek)
+#vysledek = open('basen')
+
+#obsah = vysledek.read()
+#print(obsah)
+
+#for i in vysledek:
+#    print(repr(i))
+
+#vysledek.close()
