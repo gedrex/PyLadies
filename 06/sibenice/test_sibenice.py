9256eea770d95e00c4f222ff67c0bacb341aa134
Lea Bradáčová <gedrex@gmail.com>
2016-05-25 17:00:49 +0200

    dopsane testy jak se dalo

--- 06/sibenice/test_sibenice.py
+++ 06/sibenice/test_sibenice.py
@@ -3,5 +3,12 @@
 import pytest
 from futils import *
 
-
+#is_right(char, word)
+def test_is_right():
+    assert is_right('a', 'sedm') == False
+    assert is_right('a', 'samice') == 2
+    
+#change_board(position, char, board)
+def test_change_board():
+    assert change_board(5, 'a', '----xx-----') == '----ax-----'
 
