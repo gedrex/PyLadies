80bef0d23a4067bc0d715057911e980a152e4907
Lea Bradáčová <gedrex@gmail.com>
2016-06-14 19:25:33 +0200

    Classes
    Krmeni drave zvere as. dedicnost a funkce super()

--- 11/classes2.py
+++ 11/classes2.py
@@ -2,13 +2,21 @@
 
 #definice tridy
 class Kotatko:
-    #specialni metoda, ktera aktivuje objekt a krome samu sebe, vzdy musi byt pritomen atribut jmeno
+    ###specialni metoda, ktera aktivuje objekt a krome samu sebe, vzdy musi byt pritomen atribut jmeno
     def __init__(self, jmeno):
         self.jmeno = jmeno
     
-    #dalsi specialni funkce, ktera prevadi to, co funkce vraci, na string
-    def __str__(self):
-        return '<Kotatko s jmenem: {}'.format(self.jmeno)
+    def snez(self, jidlo):
+        print('{}: {} mi chutna!'.format(self.jmeno, jidlo))
+
+    ###dalsi specialni funkce, ktera prevadi to, co funkce vraci, na string
+    #def __str__(self):
+    #    return '<Kotatko s jmenem: {}'.format(self.jmeno)
+    
+    ###dalsi specialni funkce(funguje stejne jako len(ve fcich)
+    #def __len__(self):
+    #    return 1
+
 
     #definice metody funkce
     def zamnoukej(self):
@@ -22,8 +30,18 @@ mourek = Kotatko(jmeno='Mourek')
 micka.zamnoukej()
 mourek.zamnoukej()
 
+kocour = Kotatko('Kocour')
+kocour.snez('ryba')
+print(kocour.jmeno)
+       
 #volani __str__:
-print(micka)
-print(micka.__str__())
-print(str(micka))
+#print(micka)
+#print(mourek.__str__())
+#print(str(micka))
+
+
+### dalsi specialni metoda
+#print(len(micka))
+#print(micka.__len__())
+
 
---
+++ 11/zviratka.py
@@ -0,0 +1,54 @@
+#!/home/gedrex/pyladies/venv/bin/python
+
+###
+#Python nejdriv hleda metody a atributy v podtride, pak se teprve kouka do rodicovske. Pokud funguje dedicnost, jdeme tedy odspoda nahoru.
+#proto muzeme predefinovat cele funkce
+
+class Zviratko:
+    def __init__(self, jmeno):
+        self.jmeno = jmeno
+
+    def snez(self, jidlo):
+        print('{}: {} mi chutna!'.format(self.jmeno, jidlo))
+
+
+
+#definice podtridy - dedicnost
+class Kotatko(Zviratko):
+    def zamnoukej(self):
+        print('{}: Mnau!'.format(self.jmeno))
+    
+    def snez(self, jidlo):
+        print('{} si s {} nejdriv hraje, nez to sni!'.format(self.jmeno, jidlo))    
+        #volani funkce super zavola metodu rodicovske tridy s atributy ktere zadame
+        super().snez(jidlo)
+
+
+class Stenatko(Zviratko):
+    def zastekej(self):
+        print('{}: Haf!'.format(self.jmeno))
+
+
+class Hadatko(Zviratko):
+    def __init__(self, jmeno):
+        jmeno = jmeno.replace('s', 'sss')
+        super().__init__(jmeno)
+
+
+micka = Kotatko('Kocour')
+micka.snez('ryba')
+
+puclik = Stenatko('Puclik')
+puclik.snez('ryba')
+puclik.zastekej
+
+standa = Hadatko('Stanislav')
+standa.snez('myš')
+
+
+#### Krmení dravé zvěře ####
+
+zoo = [Kotatko('Mourek'), Stenatko('Punťa'), Hadatko('stanislav')]
+
+for zviratko in zoo:
+    zviratko.snez('flákota')
