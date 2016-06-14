b20926e2de66afa43841662530b38c9577e6d33e
Lea Bradáčová <gedrex@gmail.com>
2016-06-14 18:43:24 +0200

    Classes
    Hrajeme si s koťátky(a možná i štěňátky...)

--- 11/classes2.py
+++ 11/classes2.py
@@ -5,25 +5,25 @@ class Kotatko:
     #specialni metoda, ktera aktivuje objekt a krome samu sebe, vzdy musi byt pritomen atribut jmeno
     def __init__(self, jmeno):
         self.jmeno = jmeno
+    
+    #dalsi specialni funkce, ktera prevadi to, co funkce vraci, na string
+    def __str__(self):
+        return '<Kotatko s jmenem: {}'.format(self.jmeno)
 
     #definice metody funkce
     def zamnoukej(self):
         print('{}: Mnau!'.format(self.jmeno))
 
 
-#prirazeni tridy do promene
-micka = Kotatko()
+micka = Kotatko('Micka')
 
-#prirazeni atributu tridy
-micka.jmeno = 'Micka'
+mourek = Kotatko(jmeno='Mourek')
 
-#volani metody Kotatka micka
 micka.zamnoukej()
-
-#tak ne:
-# Kotatko.zamnoukej(kotatko)
-
-mourek = Kotatko()
-mourek.jmeno = 'Mourek'
 mourek.zamnoukej()
+
+#volani __str__:
+print(micka)
+print(micka.__str__())
+print(str(micka))
 
